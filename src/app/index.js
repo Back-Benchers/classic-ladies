8f5fc0ebffd17411c1adfdc87d5d52daf9513311
fivetran-kamaleshpati <83626034+fivetran-kamaleshpati@users.noreply.github.com>
2021-11-02 15:36:39 +0530

    feature(sequilze):alter true

--- src/app/index.js
+++ src/app/index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(log.sendResponse);
 
-// table.sequelize.sync();
+// table.sequelize.sync({ alter: true })
 
 app.get('/', function(req, res) {
     res.send('Hello World');
