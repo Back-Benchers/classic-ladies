f197f2ba1384d53e0be3f8d916a863d36c84c049
Kumar Aman <kaman0353@gmail.com>
2021-11-02 23:21:18 +0530

    Added error and not found handler

--- src/app/index.js
+++ src/app/index.js
@@ -24,9 +24,25 @@ app.get('/', function(req, res) {
     res.send('Hello World');
 })
 
+function notFound(req, res, next) {
+  res.status(404);
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  next(error);
+}
+
+function errorHandler(err, req, res, next) {
+  res.status(res.statusCode || 500);
+  res.json({
+    message: err.message,
+    stack: err.stack
+  });
+}
+
+
 app.use('/api', apiClient);
 app.use('/admin', apiAdmin);
-
+app.use(notFound);
+app.use(errorHandler);
 const server = app.listen(process.env.PORT, function() {
     let host = server.address().address;
     let port = server.address().port;
